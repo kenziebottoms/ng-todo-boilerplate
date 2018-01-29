bc7aa0703dd33d809807853c4ca8ec6eaff75555
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-01-29 09:57:16 -0600

    sep index into partials, add routing wtihout ctrls

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
 "use strict";
+
+angular.module("TodoApp", ["ngRoute"])
+    .config($routeProvider => {
+        $routeProvider
+            .when("/items/list", {
+                templateUrl: "/partials/item-list.html",
+                // TODO: controller: "ItemListCtrl"
+            })
+            .when("/items/new", {
+                templateUrl: "/partials/item-form.html",
+                // TODO: controller: "NewItemCtrl"
+            })
+            // this goes at the bottom because otherwise
+            //  it would catch "list and "new" as :id
+            .when("/items/details/:id", {
+                templateUrl: "/partials/item-details.html",
+                // TODO: controller: "ItemDetailsCtrl"
+            });
+    });
